436b37e8d6fc1dfd18dd5fdb7b9d269680980adf
0Teken6 <timu.rev006@gmail.com>
2024-08-26 22:58:51 +0600

    update

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'DATABASE_URL': config("DATABASE_URL")
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
